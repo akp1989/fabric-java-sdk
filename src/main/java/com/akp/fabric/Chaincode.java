f7974558b30c4c65d22a37e12c92cdd400c85056
Prabhakaran AK <akp1989@gmail.com>
2021-11-12 18:17:46 +0000

    Commiting the changes for ripe version of the java-sdk

--- src/main/java/com/akp/fabric/Chaincode.java
+++ src/main/java/com/akp/fabric/Chaincode.java
@@ -31,58 +31,56 @@ public class Chaincode {
 
 	public static void main(String[] args) throws Exception {
 		// Load a file system based wallet for managing identities.
-		Wallet wallet = Wallet.createFileSystemWallet(Paths.get("ceadar","gatewayWalletTest"));
+		Wallet wallet = Wallet.createFileSystemWallet(Paths.get("ripe","gatewayWalletTest"));
 
 		// Load the network config path
-		Path networkConfigPath = Paths.get("ceadar", "fabric-network", "connection.yaml");
+		Path networkConfigPath = Paths.get("ripe", "fabric-network", "connection.yaml");
  
 		Gateway.Builder builder = Gateway.createBuilder();
-		builder.identity(wallet, "org1_client1_org1").networkConfig(networkConfigPath).discovery(false);
+		builder.identity(wallet,"test_client01").networkConfig(networkConfigPath).discovery(false);
 
 		// create a gateway connection
 		try (Gateway gateway = builder.connect()) {
 
 			// get the network and contract
 			Network network = gateway.getNetwork("testchannel");
- 
-			Contract contract = network.getContract("privateasset","AssetContract"); 
 			Channel channelTest = network.getChannel(); 
-			
+			Contract contract = network.getContract("privateasset","AssetContract"); 
+ 
 			//Possible invoke transactions 
-			//Transaction invokeTransaction = contract.createTransaction("assetCreate");
+			Transaction invokeTransaction = contract.createTransaction("assetCreate");
 			//Transaction invokeTransaction = contract.createTransaction("assetUpdate");
 			//Transaction invokeTransaction = contract.createTransaction("assetUpdatePrivate");
-			Transaction invokeTransaction = contract.createTransaction("assetDelete");
+			//Transaction invokeTransaction = contract.createTransaction("assetDelete");
 			
 			//Possible query transactions
-			//Transaction queryTransaction = contract.createTransaction("assetRead");
+			Transaction queryTransaction = contract.createTransaction("assetRead");
 			//Transaction queryTransaction = contract.createTransaction("assetReadPrivate");
-			Transaction queryTransaction = contract.createTransaction("getTransactionHistory");
+			//Transaction queryTransaction = contract.createTransaction("getTransactionHistory");
 
 			byte[] result;
 			
 			Map<String,byte[]> transientMap = new HashMap<>();
-			Encoder encoder = Base64.getEncoder();
-			String transientString = new String();
+ 			String transientString = new String();
 			
-			transientString = "gatewayprice00";	
+			transientString = "secretprice";	
 			transientMap.put("assetPrice",transientString.getBytes());
 
 			
 			
-			invokeTransaction.setTransient(transientMap);
-			//result = invokeTransaction.submit("assetID:gateway002","org1","called from org1");
+//			invokeTransaction.setTransient(transientMap);
+//			result = invokeTransaction.submit("testasset1","test","called from test");
 			//result = invokeTransaction.submit("assetID:gateway001","price001");
-			result = invokeTransaction.submit("assetID:gateway002");
-			System.out.println(new String(result));
+//			result = invokeTransaction.submit("assetID:gateway002");
+//			System.out.println(new String(result));
 			
-			result = queryTransaction.submit("assetID:gateway002");
+			result = queryTransaction.submit("testasset1");
 			System.out.println(new String(result));
 			
 
-//			BlockInfo blockInfo = channelTest.queryBlockByTransactionID("9de043f5d5f6c4a3aa52494c4c504c0845bb1a70d5c3a0b1b20a807eeef0040b");
-//			BlockchainUtil blockchainUtil = new BlockchainUtil();
-//			System.out.println("The transaction for current hash is :" + blockchainUtil.processBlock(blockInfo));
+			BlockInfo blockInfo = channelTest.queryBlockByTransactionID("1c58c29b91f64f0ba7ac3975415fa6183a398ce6c81732e70a75f54fbb78623b");
+			BlockchainUtil blockchainUtil = new BlockchainUtil();
+			System.out.println("The transaction for current hash is :" + blockchainUtil.processBlock(blockInfo));
 		}
 	}
 
--- src/main/java/com/akp/fabric/RegisterEnroll.java
+++ src/main/java/com/akp/fabric/RegisterEnroll.java
@@ -26,46 +26,64 @@ public class RegisterEnroll {
 		// Create a CA client for interacting with the CA.
 		Properties properties = new Properties();
 		/* Properties to pass the TLS CA certificates for SSL connection */
-		properties.put("pemFile","ceadar/crypto/orga/msp/cacerts/ca-cert.pem");
-		//properties.put("pemFile","ceadar/ca-cert.pem");
-		properties.put("allowAllHostNames", "true");
-		CAClient caClient = new CAClient("https://192.168.0.175:7010", properties);
+		properties.put("pemFile","ripe/crypto/root/msp/tlscacerts/tlsca-cert.pem");
+ 		properties.put("allowAllHostNames", "true");
+		CAClient caClient = new CAClient("https://localhost:17720", properties);
 		
 		// Create a wallet for managing identities
-		Wallet wallet = Wallet.createFileSystemWallet(Paths.get("ceadar","gatewayWalletTest"));
+		Wallet wallet = Wallet.createFileSystemWallet(Paths.get("ripe","gatewayWalletTest"));
 		//Wallet wallet = Wallet.createFileSystemWallet(Paths.get("ceadar","test","wallet"));
 		// Check to see if we've already enrolled the admin user.
-		boolean adminExists = wallet.exists("orga_caadmin"); 
+		boolean adminExists = wallet.exists("root_caadmin"); 
 		if (!adminExists) {
 			//If not enroll the admin user
-			Enrollment enrollment = caClient.enrollUser("admin", "adminroot",new EnrollmentRequest(), null); 
-			Identity caadmin = Identity.createIdentity("OrgAMSP", enrollment.getCert(),enrollment.getKey());
-		    wallet.put("orga_caadmin", caadmin); 
+			Enrollment enrollment = caClient.enrollUser("icadmin.boot", "icadminpw",new EnrollmentRequest(), null); 
+			Identity caadmin = Identity.createIdentity("RootMSP", enrollment.getCert(),enrollment.getKey());
+		    wallet.put("root_caadmin", caadmin); 
 		 } 
-		 Identity adminIdentity = wallet.get("orga_caadmin");
+		 Identity adminIdentity = wallet.get("root_caadmin");
 		 //Set the admin user context with which we will register new ID's
 		 UserContext adminUser = new UserContext(); adminUser.setName("admin");
 		 adminUser.setIdentity(adminIdentity);
 		 adminUser.setMspId(adminIdentity.getMspId()); 
 		 
-		 boolean userExists = wallet.exists("orgb_client01");
+//		 boolean userExists = wallet.exists("test_client01");
+//		 if(userExists)
+//			 wallet.remove("test_client01");
+//
+//		 String responsesecret = caClient.registerUser("testclient01","test","client",adminUser,null);
+//		 System.out.println(responsesecret);
+//    
+//	     // Enroll the User and add it to wallet
+//		  EnrollmentRequest enrollmentRequest = new EnrollmentRequest();
+//		  enrollmentRequest.addHost("127.0.0.1");
+//		  enrollmentRequest.addHost("localhost"); 
+//		  enrollmentRequest.addHost("0.0.0.0");
+//		  enrollmentRequest.addHost("*.test.ripe.io");
+//		  //enrollmentRequest.setProfile("tls");
+//		  Enrollment enrollment = caClient.enrollUser("testclient01", responsesecret, enrollmentRequest, null);
+//		  Identity user = Identity.createIdentity("TestMSP", enrollment.getCert(),enrollment.getKey()); 
+//		  wallet.put("test_client01", user); 
+//		  System.out.println(enrollment.getCert().toString());
+		  
+		  
+		 boolean userExists = wallet.exists("root_client01");
 		 if(userExists)
-			 wallet.remove("orgb_client01");
+			 wallet.remove("root_client01");
 
-		 //String responsesecret = caClient.registerUser("orgbclient01","orgb","client",adminUser,null);
-		 //System.out.println(responsesecret);
+		 String responsesecret = caClient.registerUser("rootclient01","root","client",adminUser,null);
+		 System.out.println(responsesecret);
     
 	     // Enroll the User and add it to wallet
 		  EnrollmentRequest enrollmentRequest = new EnrollmentRequest();
 		  enrollmentRequest.addHost("127.0.0.1");
-		  enrollmentRequest.addHost("localhost");
-		  enrollmentRequest.addHost("192.168.0.175");
+		  enrollmentRequest.addHost("localhost"); 
 		  enrollmentRequest.addHost("0.0.0.0");
-		  enrollmentRequest.addHost("*.orgb.akp.com");
+		  enrollmentRequest.addHost("*.root.ripe.io");
 		  //enrollmentRequest.setProfile("tls");
-		  Enrollment enrollment = caClient.enrollUser("orgbclient01", "wZVzWLKHSgFp", enrollmentRequest, null);
-		  Identity user = Identity.createIdentity("OrgBMSP", enrollment.getCert(),enrollment.getKey()); 
-		  wallet.put("orgb_client01", user); 
+		  Enrollment enrollment = caClient.enrollUser("rootclient01", responsesecret, enrollmentRequest, null);
+		  Identity user = Identity.createIdentity("RootMSP", enrollment.getCert(),enrollment.getKey()); 
+		  wallet.put("root_client01", user); 
 		  System.out.println(enrollment.getCert().toString());
 	}
 }
